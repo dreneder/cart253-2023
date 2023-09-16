e9c6827dc4edb29d3de86e1c1d2f661bc68423ea
dreneder <144075267+dreneder@users.noreply.github.com>
2023-09-16 17:14:11 -0400

    learned to change variables

--- activities/variables/js/script.js
+++ activities/variables/js/script.js
@@ -15,15 +15,18 @@ function preload() {
 }
 
 
-// defining a variable it can either be as bellow or under settup as circleSize = 100;
+// defining a variable it can either be as bellow or under settup or draw as circleSize = 200;
 // variable names must be named continiguosly, like one name with no spaces, avoid numbers or weird simbols
 // if there is a difference of lower X upper case javascript will respect that, so make sure it follows the exact name
 
 
 let backgroundShade = 0;
-let cricleX = 250;
+let circleX = 0;
 let circleY = 250;
-let circleSize = 100;
+let circleSize = 200;
+//in programming, rather than subtracting values it might be better to use negative numbers
+let circleSpeed = 2;
+let circleAcceleration = 0.25;
 
 
 function setup() {
@@ -31,10 +34,15 @@ createCanvas(500, 500);
 }
 
 
-/**
- * Description of draw()
-*/
+// variables can be altered as seen below
+// the += can be used as a way to add the value of the variable to itself
+
+
 function draw() {
+    
+
 background(backgroundShade);
+circleX += circleSpeed;
+circleSpeed = circleSpeed + circleAcceleration;
 ellipse(circleX, circleY, circleSize);
 }
