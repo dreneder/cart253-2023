ff97c5d3d8859601655e0d3e5e7d8f8b9c6c3beb
André Neder <144075267+dreneder@users.noreply.github.com>
2023-09-23 11:59:03 -0400

    4.1 started learning Conditionals

---
+++ classes/conditionals/js/script.js
@@ -0,0 +1,31 @@
+/**
+ * Learning conditionals
+ * Andre Neder
+ * 
+ * This was started to learn conditionals
+ */
+
+"use strict";
+
+/**
+ * Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+ * Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+ * Description of draw()
+*/
+function draw() {
+
+}
