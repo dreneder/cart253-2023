2460c9c09f5b19ff45dcffc049a633bbafd7ba85
André Neder <144075267+dreneder@users.noreply.github.com>
2023-09-23 17:44:15 -0400

    4.11 finished time

--- classes/image_time_debugging/js/script.js
+++ classes/image_time_debugging/js/script.js
@@ -1,11 +1,21 @@
+// time starts from a blank script
 
+let clown = {
+    x: 250,
+    y: 250,
+    size: 100,
+    image: undefined
+};
 
-"use strict";
-
-let clownImage;
+// set for images
+// let clownImage;
 
 function preload() {
-    clownImage = loadImage("assets/images/clown.png");
+    clown.image = loadImage("assets/images/clown.png");
+
+
+// set for images
+//     clownImage = loadImage("assets/images/clown.png");
 }
 
 
@@ -13,10 +23,24 @@ function setup() {
     createCanvas(500,500);
 }
 
+
 function draw() {
     background(0);
 
-    imageMode(CENTER);
+// set for images
+//     imageMode(CENTER);
 // image takes the the image loaded, positionX, positionY, scaleX and scaleY
-    image(clownImage,mouseX,mouseY,50,50);
+    // image(clownImage,mouseX,mouseY,50,50);
+
+// set for time
+    clown.x = mouseX;
+    clown.y = mouseY;
+
+    imageMode(CENTER);
+    image(clown.image,clown.x,clown.y,clown.size,clown.size);
+}
+
+// in time it's explained that this is an event function
+function mousePressed() {
+    clown.size = clown.size +50;
 }
