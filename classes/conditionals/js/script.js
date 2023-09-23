ae8fdba912dd0678cb34dcaade45e74e35bcd9a4
André Neder <144075267+dreneder@users.noreply.github.com>
2023-09-23 12:23:31 -0400

    4.1 finished conditionals 1

--- classes/conditionals/js/script.js
+++ classes/conditionals/js/script.js
@@ -7,25 +7,48 @@
 
 "use strict";
 
-/**
- * Description of preload
-*/
 function preload() {
 
 }
 
-
-/**
- * Description of setup
-*/
-function setup() {
-
+let backgroundShade = 0;
+let circle = {
+    x: 0,
+    y: 250,
+    size: 100,
+    speed: 5
 }
 
 
-/**
- * Description of draw()
-*/
+function setup() {
+    createCanvas(500, 500);
+}
+
+
 function draw() {
+    background(backgroundShade);
+    
+    circle.x = circle.x + circle.speed;
+
+    if (circle.x > width) {
+        circle.speed = -circle.speed;
+    }
+
+    if (circle.x < 0) {
+        circle.speed = -circle.speed;
+    }
+// possibilities here are >, <, >=, <=, and the followings   
+// use 3 "=" (===) if you want ot check wether two things are equal to each other
+// use "!==" means does not equal
+
+if (mouseY < height/2) {
+    fill(255, 0, 0);
+}
+if (mouseY > height/2) {
+    fill(0, 0, 255);
+}
+
+
+    ellipse(circle.x,circle.y,circle.size);
 
 }
