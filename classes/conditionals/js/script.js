3a75103cdc679a18f7fc942f94020fcc4a61c3de
André Neder <144075267+dreneder@users.noreply.github.com>
2023-09-23 12:52:04 -0400

    4.2 finished conditions 2

--- classes/conditionals/js/script.js
+++ classes/conditionals/js/script.js
@@ -16,7 +16,7 @@ let circle = {
     x: 0,
     y: 250,
     size: 100,
-    speed: 5
+    speed: 1
 }
 
 
@@ -40,13 +40,48 @@ function draw() {
 // // possibilities here are >, <, >=, <=, and the followings   
 // // use 3 "=" (===) if you want ot check wether two things are equal to each other
 // // use "!==" means does not equal
-
 // if (mouseY < height/2) {
 //     fill(255, 0, 0);
 // }
-// if (mouseY > height/2) {
+
+// here you can have as many else if's as you want, note that the "mouseX < 2 * width/3" means 2/3 of the value
+// if (mouseX < width/3) {
+//     fill(255, 0, 0);
+// }
+// else if (mouseX < 2 * width/3) {
+//     fill(0, 255, 0);
+// }
+// else {
 //     fill(0, 0, 255);
 // }
+
+// the if inside means another conditional inside the first one (a question followed by a subquestion)
+//then it needs both conditions to be true so the second if means and
+// fill(255,255,255);
+// if (circle.x > width/3) {
+//     if (circle.x < 2 * width/3) {
+//         fill(255,0,0);
+//     }
+// }
+
+// this does exactly the same thing but is how you actually declares an "and" statement to the code
+// fill(255,255,255);
+// if (circle.x > width/3 && circle.x < 2 * width/3) {
+//     fill(255,0,0);
+// }
+
+// the following "||" means or, so it only turns read under these conditions
+// fill(255,255,255);
+// if (circle.x < width/3 || circle.x > 2 * width/3) {
+//     fill(255,0,0);
+// }
+
+// the following ! means not (not true) and it needs to be under a parantesis of it's own
+fill(255,255,255);
+if (!(circle.x < width/3)) {
+    fill(255,0,0);
+}
+
 
 
     ellipse(circle.x,circle.y,circle.size);
