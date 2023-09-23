0a1ab32d3f3b4bc9f92af4aab993918ad69a85d2
André Neder <144075267+dreneder@users.noreply.github.com>
2023-09-23 15:13:45 -0400

    4.5 finished mouse input

--- classes/mouse_input_&_movement/js/script.js
+++ classes/mouse_input_&_movement/js/script.js
@@ -5,12 +5,35 @@ function preload() {
 
 }
 
+let bg = {
+    r: 0,
+    g: 0,
+    b: 0
+}
+
+let circle = {
+    x: 250,
+    y: 250,
+    size: 100
+}
 
 function setup() {
-
+    createCanvas (500,500);
 }
 
 
 function draw() {
+    background(bg.r,bg.g,bg.b);
 
+    ellipse(circle.x,circle.y,circle.size);
+}
+
+    // function mousePressed() {
+    
+    // function mouseMoved() {
+
+    function mouseDragged() {
+    bg.r = random(0,255);
+    bg.g = random(0,255);
+    bg.b = random(0,255);
 }
