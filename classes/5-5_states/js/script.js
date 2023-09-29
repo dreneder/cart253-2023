b7cbce931ed346e8225cfba50855a1be420dcfc2
André Neder <144075267+dreneder@users.noreply.github.com>
2023-09-29 19:24:55 -0400

    5.5 finished states

--- classes/5-5_states/js/script.js
+++ classes/5-5_states/js/script.js
@@ -1,14 +1,67 @@
+let circle = {
+    x: 0,
+    y: 250,
+    size: 100,
+    vx: 0,
+    vy: 0,
+    speed: 2
+};
 
-function preload() {
 
-}
+let state = `title`; // Possible states are: title, animation, ending
 
 
-function setup() {
 
-}
-
+function setup () {
+        createCanvas (500,500);
+        circle.vx = circle.speed;
+        textSize(32);
+        textAlign(CENTER,CENTER);
+    }
 
 function draw() {
+        background (0);
 
-}
+
+        if (state === `title`) {
+            title();
+        }
+        else if (state === `animation`) {
+            animation();
+        }
+        else if (state === `ending`) {
+            ending();
+        }
+    }
+
+
+
+    function keyPressed() {
+        if (state === `title`) {
+        state = `animation`;
+        }
+    }
+
+    function title() {
+          // Title
+    fill(255);
+    text(`Life.`,width/2,height/2);
+    }
+
+    function animation() {
+            // Animation
+            circle.x = circle.x + circle. vx;
+            circle.y = circle.y + circle.vy;
+    
+            if (circle.x > width) {
+                state = `ending`;
+            }
+            
+            ellipse(circle.x,circle.y,circle.size);
+    }
+
+    function ending() {
+            // Ending
+            fill(127);
+            text(`It's all over.`,width/2,height/2);
+    }
