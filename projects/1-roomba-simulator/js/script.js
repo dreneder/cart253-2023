2cb162dd67037eafb350aa82c55fa4050f42f733
dreneder <144075267+dreneder@users.noreply.github.com>
2023-10-06 19:51:18 -0400

    something

---
+++ projects/1-roomba-simulator/js/script.js
@@ -0,0 +1,31 @@
+/**
+ * 2023 Roomba Simulator
+ * André Neder
+ * 
+ * Folowing the success of 
+ */
+
+"use strict";
+
+/**
+ * Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+ * Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+ * Description of draw()
+*/
+function draw() {
+
+}
