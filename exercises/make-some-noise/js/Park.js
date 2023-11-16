ce2113caabf3445f6a81eb24641a3dc31507b594
André Neder <144075267+dreneder@users.noreply.github.com>
2023-11-16 14:01:59 -0500

    gave up on the exercise

--- exercises/make-some-noise/js/Park.js
+++ exercises/make-some-noise/js/Park.js
@@ -1,39 +1,50 @@
-class Ignition {
+class HandBrake {
     constructor(x,y) {
         this.x = x;
         this.y = y;
         this.button = 1; // for size
+        this.r = 130;
+        this.g = 130;
+        this.b = 130;
     }
 
 pressed() {
     // defines if the mouse is over the button
     let onButton = collidePointCircle(mouseX,mouseY,this.x,this.y,80*this.button)
-    if (onButton === true) {startStop = true;}
     // activates the ignition
     if (mouseOn === true && onButton === true) {
+        pButton = true;
         this.button = 0.75; // changes the size
     }
-    else {
+    else { 
         this.button = 1;
+    }
+
+    if (parkOn === true) {
+        this.r = 250;
+        this.g = 160;
+        this.b = 50;
+    }
+    else {
+        this.r = 130
+        this.g = 130
+        this.b = 130
     }
 }
 
 display() { //draws the button with text inside
     push();
-    stroke(255,0,0);
-    strokeWeight(8*this.button);
-    fill(255);
-    ellipse(this.x,this.y,80*this.button);
+    stroke(this.r,this.g,this.b);
+    strokeWeight(6*this.button);
+    fill(80);
+    ellipse(this.x,this.y,75*this.button);
 
     noStroke();
-    fill(0);
+    fill(this.r,this.g,this.b);
     textAlign(CENTER,CENTER);
     textStyle(BOLD);
-    textSize(10*this.button);
-    text(`ENGINE`,this.x,this.y-15);
-    textSize(15*this.button);
-    text(`START`,this.x,this.y);
-    text(`STOP`,this.x,this.y+15);
+    textSize(50*this.button);
+    text(`P`,this.x,this.y+5);
     pop();
 }
 }
--- exercises/make-some-noise/js/Tachometer.js
+++ exercises/make-some-noise/js/Tachometer.js
@@ -3,9 +3,19 @@ class Tachometer {
         this.x = x;
         this.y = y;
         this.size = 220;
+        this.needle = undefined; // for angle
     }
 
+move(){
+    //moves the needle according to the RPM
+    this.needle = map(rpm,0,7000,149,359);
+}
+
 display() {
+    // defines
+    angleMode(DEGREES);
+    
+    // draws the main gauge cluster
     push();
     translate(this.x,this.y);
     fill(255);
@@ -13,8 +23,41 @@ display() {
     strokeWeight(5);
     ellipse(0,0,this.size);
     noStroke();
+    
+    // draws text of RPM
+    fill(0);
+    textAlign(CENTER,CENTER);
+    textStyle(BOLD);
+    textSize(40);
+    text(`RPM`,0,50);
+    textSize(15);
+    text(`x1000`,0,75);
+    
+    
+    // rotate for the angle of the first level
+    rotate(-45);
+    //draws the levels of gauge
+    for (let i = 0; i < 8; i++) {
+        fill(0);
+        rotate(30); // rotate at each addition
+        rect(-110,0,15,3);
+        push();
+        translate(-85,0); //draws number above the first level
+        rotate(15+i*-30); // counter rotates each number so it's allingned
+        textSize(20);
+        text(i,0,0+1); // writes number according to loop
+        pop();
+    }
+    
+    // draws the needle
+    rotate(this.needle);
+    fill(100,100,255);
+    triangle(0,3,-100,0,0,-3);
+    
+    // center circle
     fill(60);
-    ellipse(0,0,this.size*0.05)
+    ellipse(0,0,12);
+      
     pop();
 }
 }
