604297abba3c1e4573e6b5c704cb9ece3f61147a
dreneder <144075267+dreneder@users.noreply.github.com>
2023-11-28 05:38:06 -0500

    after so much trying realized how to activate sprites alpha

--- projects/project-2/prototype/smoke/js/script.js
+++ projects/project-2/prototype/smoke/js/script.js
@@ -16,7 +16,7 @@ let spaceShipImg;
 let stage2Img;
 let rocketImg;
 
-
+let cameraPan;
 
 let spaceShip;
 let stage2;
@@ -26,6 +26,11 @@ let rocket;
 let ground;
 
 let groundDetail;
+
+let dock1;
+let dock2;
+
+let testSprite;
 
 let stageCounter = 0; // variable to keep each action separate
 
@@ -38,43 +43,60 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
-  rocket = new Sprite(width/2,height/4*3);
-  ground = new Sprite(width/2,height/16*15,width,height-height/8*7,'static');
+  
+  stage2 = new Sprite(width/2,height-384);
+  spaceShip = new Sprite(width/2,height-505);
+  rocket = new Sprite(width/2,height-225);
+  ground = new Sprite(width/2,height-75,width,150,'static');
   
   ground.color = '#00bd3f';
-
-
-  for (let i = 0; i < 500; i++) {
-  groundDetail = new Sprite(random(0,width),
-                            random(height,ground.y-height/16),
-                            1,1, 'static')
-}
-
-  rocket.img = spaceShipImg;
-  rocket.scale = 0.5;
-
   
+  
+  for (let i = 0; i < 500; i++) {
+    groundDetail = new Sprite(random(0,width),
+    random(height,ground.y-height/16),
+    1,1, 'static')
+  }
+  
+  spaceShip.img = spaceShipImg;
+  stage2.img = stage2Img;
+  rocket.img = rocketImg;
+  rocket.scale = 0.2;
+  spaceShip.scale = 0.2;
+  stage2.scale = 0.2;
+  // spaceShipImg.setAlpha(200);
+  
+  dock1 = new GlueJoint(rocket,stage2);
+  dock2 = new GlueJoint(stage2,spaceShip);
+  
+  let myColor = color(255,0,0,150);
+  
+  testSprite = createSprite(width/2,height/2);
+  testSprite.color = myColor;
+ 
   
   // rocket.visible = false;
   // ground.visible = false;
 }
 
 function draw() {
+  clear();
   background(183, 226, 247);
+  
 
   launch(); // function to enable the stageCounter
 
   
   
-  camera.zoomTo(0.5);
-  if (rocket.position.y <= height/2) {
+  camera.zoomTo(1);
+  // if (rocket.position.y <= height/2) {
     camera.y = rocket.y;
-  }
+
+  // }
   
   
   push();
-  translate(camera.position.x,camera.position.y);
+  translate(width/2-rocket.position.x,height/2-rocket.position.y+100); 
   
   
   
@@ -82,7 +104,7 @@ function draw() {
   // rocket.x = width/2; // defining rocket x position (won't change)
   // if (stageCounter >= 1) { // once stages advance
   for (let i=0; i<5; i++){
-    let p = new Particle(1, 1, rocket.x, rocket.y); // loop creates particles
+    let p = new Particle(1, 1, rocket.position.x, rocket.position.y); // loop creates particles
     smoke.push(p);
     smoke[i].liftOff();
   }
@@ -101,6 +123,8 @@ function draw() {
   console.log(rocket.position.y);
   
   pop();
+  
+
   
 }
 
---
+++ projects/project-2/prototype/test_p5play/sketch.js
@@ -0,0 +1,13 @@
+let ball;
+
+function setup() {
+	new Canvas(500, 500);
+
+	ball = new Sprite();
+	ball.diameter = 50;
+}
+
+function draw() {
+	background('gray');
+	drawSprites();
+}
